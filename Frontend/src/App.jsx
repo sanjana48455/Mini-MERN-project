1e682fb9d35e563564116a44e4db05ffc2bd1f73
sanjana48455 <kssanjana288@gmail.com>
2026-09-11 20:55:43 +0530

    Set create post as home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,7 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import CreatePost from './pages/CreatePost'
 import Feed from './pages/feed'
-
-const Home = () => {
-  return (
-    <section style={{
-      minHeight: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "#f5f5f5"
-    }}>
-      <h1>Welcome to My Mini MERN Project</h1>
-
-      <div style={{ display: "flex", gap: "15px" }}>
-        <Link to="/create-post">
-          <button>Create Post</button>
-        </Link>
-
-        <Link to="/feed">
-          <button>View Feed</button>
-        </Link>
-      </div>
-    </section>
-  )
-}
 
 const App = () => {
   return (
@@ -35,7 +10,7 @@ const App = () => {
 
         <Route
           path="/"
-          element={<Home />}
+          element={<CreatePost />}
         />
 
         <Route
@@ -46,11 +21,6 @@ const App = () => {
         <Route
           path="/feed"
           element={<Feed />}
-        />
-
-        <Route
-          path="/about"
-          element={<h1>hello world</h1>}
         />
 
       </Routes>
